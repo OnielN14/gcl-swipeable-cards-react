f6b439842d2856df3148148ca401ad45390049b9
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2021-02-08 17:58:35 +0700

    (not) initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
+
+function getPointerX(evt: TouchEvent | MouseEvent): number {
+  return (evt as MouseEvent).pageX || (evt as TouchEvent).touches[0].pageX
+}
 
 function App() {
+  const [ people, setPeople ] = React.useState([
+    'Jamal', 'Komar', 'Badrun', 'Kurniadi', 'Yahya', 'Tatang'
+  ])
+
+  const swipeTarget = React.useRef<HTMLElement>()
+  const swipeTargetBCR = React.useRef<DOMRect>()
+  const startX = React.useRef<number>(0)
+  const currentX = React.useRef<number>(0)
+  const targetX = React.useRef<number>(0)
+  const screenX = React.useRef<number>(0)
+  const isDragging = React.useRef(false)
+
+  function onStart (evt: TouchEvent | MouseEvent) {
+    if (!(evt.target as HTMLElement).classList.contains(style.Card)) return;
+
+    isDragging.current = true
+    swipeTarget.current = evt.target as HTMLElement
+    swipeTargetBCR.current = swipeTarget.current.getBoundingClientRect()
+    startX.current = getPointerX(evt)
+    currentX.current = startX.current
+
+    swipeTarget.current!.style.willChange = 'transform'
+  }
+
+  function onMove (evt: TouchEvent | MouseEvent) {
+    if (!swipeTarget.current) return
+
+    currentX.current = getPointerX(evt)
+  }
+
+  function onEnd (evt: TouchEvent | MouseEvent) {
+    if (!swipeTarget.current) return
+
+    targetX.current = 0
+    if (Math.abs(screenX.current) > swipeTargetBCR.current!.width * 0.35) {
+      targetX.current = screenX.current > 0 ? swipeTargetBCR.current!.width : -swipeTargetBCR.current!.width 
+    }
+
+    swipeTarget.current!.style.willChange = ''
+    isDragging.current = false
+  }
+  
+  function animationUpdate () {
+    requestAnimationFrame(animationUpdate)
+
+    if (!swipeTarget.current) return
+
+    if (isDragging.current) {
+      screenX.current = currentX.current - startX.current
+    } else {
+      screenX.current += (targetX.current - screenX.current) / 10
+    }
+
+
+    swipeTarget.current!.style.transform = `translateX(${screenX.current}px)`
+  }
+
+  React.useEffect(() => {
+
+    document.addEventListener('touchstart', onStart)
+    document.addEventListener('touchmove', onMove)
+    document.addEventListener('touchend', onEnd)
+
+    requestAnimationFrame(animationUpdate)
+
+    return function cleanUp () {
+      document.removeEventListener('touchstart', onStart)
+      document.removeEventListener('touchmove', onMove)
+      document.removeEventListener('touchend', onEnd)
+    }
+  },[ people ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.Container}>
+      {
+        people.map((name, index) => (<div key={index} className={style.Card}>{name}</div>))
+      }
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.scss' {
+  const styles: Record<string, any>
+  export default styles;
+}
+
+declare module '*.css' {
+  const styles: Record<string, any>
+  export default styles;
+}
